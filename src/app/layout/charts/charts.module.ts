4f1ffaa35a0d143ecba373cc87b499340a263be0
nsaravanas <saravanahacker@gmail.com>
2018-03-12 09:14:17 +0530

    gauge chart added

--- src/app/layout/charts/charts.module.ts
+++ src/app/layout/charts/charts.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsModule as Ng2Charts } from 'ng2-charts';
-
+import { GaugeModule } from 'angular-gauge';
 import { ChartsRoutingModule } from './charts-routing.module';
 import { ChartsComponent } from './charts.component';
 import { PageHeaderModule } from '../../shared';
 
 @NgModule({
-    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule],
+    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule, GaugeModule.forRoot()],
     declarations: [ChartsComponent]
 })
-export class ChartsModule {}
+export class ChartsModule { }
